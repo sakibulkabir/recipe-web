18e3b8ba46f1de5c4e42290bb41c190b4d5cf31a
Risat <113694977+sakibulkabir@users.noreply.github.com>
2023-09-27 02:20:36 +0600

    this is a recipe website

---
+++ js/project4.js
@@ -0,0 +1,50 @@
+// var input = document.querySelector("input");
+// input.addEventListener("focus", function(){
+//     input.style.backgroundColor="red";
+//     input.style.padding="2rem";
+// });
+
+// input.addEventListener("focusout", function(){
+//     input.style.backgroundColor="green";
+//     input.style.padding="0rem";
+// });
+
+
+// var div = document.querySelector("div");
+// var p = document.querySelector("p");
+// p.addEventListener("dragstart", function(e){
+//     e.dataTransfer.setData("Text" , e.target.id);
+// });
+// div.addEventListener("dragover", function(e){
+//     e.preventDefault();
+// });
+// div.addEventListener("drop", function(){
+//     let id = e.dataTransfer.getData("Text");
+//     console.log(id)
+//      div.appendChild(document.getElementById(id));
+//      e.preventDefault();
+// });
+
+// console.clear();
+
+
+
+
+
+// const getElement = (selector) =>{
+//     const element = document.querySelector(selector);
+//     if(element) return element
+//     throw Error('Please double click your class name, there is no ${selector} class'
+//     )
+// }
+// const links = getElement('.nav-links')
+// const navBtnDom = getElement('.nav-btn')
+// navBtnDom.addEventListener('click', ()=>{
+//     links.classList.toggle('show-links')
+// })
+
+// const date = getElement('#date');
+// const currentYear = new Date().getFullYear();
+// date.textContent = currentYear;
+
+console.log('Risnat')
